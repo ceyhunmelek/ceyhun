c375e6b94a8e9fb0dc24a9a29e3936b334eccfae
Ceyhun Melek <ceyhunulasmelek@gmail.com>
2021-02-15 11:20:56 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { NextSeo } from "next-seo"
 
 export default function IndexPage() {
   return (<>
-  <NextSeo title="Ceyhun Melek - Fullstack Developer" description="Too lazy to write something"/>
+  <NextSeo title="Ceyhun Melek - Frontend Developer" description="Too lazy to write something"/>
   <div className="flex flex-wrap h-screen content-center justify-center">
     <div className="max-w-lg bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="md:flex flex-col sm:flex-row">
