65175e519d84122ac0eccbd39e313fa0995b0860
Ceyhun Melek <ceyhunulasmelek@gmail.com>
2021-04-08 14:14:38 +0300

    Purged next app

--- components/Card.js
+++
@@ -1,7 +0,0 @@
-export default function IndexPage() {
-  return (
-    <div className="md:flex-shrink-0 md:w-1/3 w-full">
-      <img className="rounded object-cover w-full h-auto md:h-full" src={"/images/pp.jpg"} alt="Ceyhun" />
-    </div>
-  )
-}
