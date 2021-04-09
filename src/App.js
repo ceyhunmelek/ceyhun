1ffd2549f95492b387fb331b4b051e985b3bdfa0
Ceyhun Melek <ceyhunulasmelek@gmail.com>
2021-04-09 15:55:17 +0300

    Completed user card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Card from "./components/Card";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Footer from "./components/Footer";
 
 const App = () => <>
   <Card />
-  <About />
-  <Skills />
-  <Projects />
-  <Footer />
 </>;
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,2 +1,9 @@
-const Card = () => <div>Card</div>;
+import Social from "./Social";
+
+const Card = () => <div className="card">
+  <img src="https://avatars.githubusercontent.com/u/19683763" alt="Ceyhun Melek" className="photo"/>
+  <p className="name">Ceyhun Melek</p>
+  <p className="title">Full Stack Developer</p>
+  <Social />
+</div>;
 export default Card;
