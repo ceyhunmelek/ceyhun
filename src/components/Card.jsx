b92ec57bab54e775e87f6bcd4ff90ba7a88413b8
Ceyhun Melek <ceyhunulasmelek@gmail.com>
2021-04-09 22:50:02 +0300

    Changed fonts in card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,8 @@ import Social from "./Social";
 
 const Card = () => <div className="card">
   <img src="https://avatars.githubusercontent.com/u/19683763" alt="Ceyhun Melek" className="photo"/>
-  <p className="name">Ceyhun Melek</p>
-  <p className="title">Full Stack Developer</p>
+  <p className="name font-poppins font-medium">Ceyhun Melek</p>
+  <p className="title font-open-sans">Full Stack Developer</p>
   <Social />
 </div>;
 export default Card;
