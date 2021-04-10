fd261ca2467fcae7a3dc9074808ab8c95e925ffc
Ceyhun Melek <ceyhunulasmelek@gmail.com>
2021-04-10 22:04:56 +0300

    Completed about section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Card from "./components/Card";
+import About from "./components/About";
+import Projects from "./components/Projects";
 
 const App = () => <>
   <Card />
+  <About />
+  <Projects />
 </>;
 
 export default App;
