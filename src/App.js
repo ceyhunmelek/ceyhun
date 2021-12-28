83b2b151cf8a966e1076f0f01da2e8f4a804fdfc
Ceyhun Melek <ceyhunulasmelek@gmail.com>
2021-12-28 09:43:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Card from "./components/Card";
-import About from "./components/About";
-import Projects from "./components/Projects";
 
 const App = () => <>
   <Card />
